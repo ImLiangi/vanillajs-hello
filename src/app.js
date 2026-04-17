9386ee175c756c3def2cdb93f6df2d629776d1cc
ImLiangi <sagliangi711@gmail.com>
2026-04-17 09:33:38 +0000

    Done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,44 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the', 'our', 'my', 'your', 'his', 'her', 'its', 'their'];
+let adj = [
+  'great',
+  'big',
+  'small',
+  'tiny',
+  'huge',
+  'fantastic',
+  'amazing',
+  'incredible',
+];
+let noun = [
+  'jogger',
+  'racoon',
+  'developer',
+  'designer',
+  'manager',
+  'teacher',
+  'student',
+  'artist',
+];
+let extensions = [
+  '.com',
+  '.net',
+  '.org',
+  '.io',
+  '.tech',
+  '.dev',
+  '.app',
+  '.site',
+];
+for (let index = 0; index < pronoun.length; index++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extensions.length; l++) {
+        console.log(pronoun[index] + adj[j] + noun[k] + extensions[l]);
+      }
+    }
+  }
+}
+}
